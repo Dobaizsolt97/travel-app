ddf637a7db7a904bff5768dc70465e02638fe42b
Dobai Zsolt <dobaizsolt97@gmail.com>
2020-03-14 21:55:58 +0200

    initialized UpdateUi function that updates page content with stored data

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,13 @@
 import "./styles/base.scss";
 import { getCityInfo } from "./js/getCityInfo";
 import { timeInterval } from "./js/timeInterval";
+import fetch from "node-fetch";
 
 const submitBtn = document.getElementById("submit");
+const destination = document.getElementById("destination");
+const img = document.querySelector(".image-holder");
+const image = document.getElementById("image");
+const description = document.getElementById("description");
 submitBtn.addEventListener("click", event => {
   event.preventDefault();
   const city = document.getElementById("city").value;
@@ -10,5 +15,17 @@ submitBtn.addEventListener("click", event => {
   if (city && date) {
     getCityInfo(city, date);
     timeInterval(date);
+    setTimeout(retriveData, 900);
   }
 });
+async function retriveData() {
+  const data = await fetch("http://localhost:8080/travel-info");
+  const info = await data.json();
+  setTimeout(updateUi(info), 400);
+}
+
+function updateUi(information) {
+  img.style.backgroundImage = `url("${information.image.imageLink}")`;
+  //image.src = `${information.image.imageLink}`;
+  description.innerText = `${information.city}, ${information.country}`;
+}
