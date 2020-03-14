d72ed337bfe2a1737270316d62240393a85d0c85
Dobai Zsolt <dobaizsolt97@gmail.com>
2020-03-14 15:06:30 +0200

    wrote functions to make calls to darksky api and pixabay api

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,11 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const request = require("request");
+const fetch = require("node-fetch");
+const pixabayBase = "https://pixabay.com/api/?key=";
 
 //defining the port
 const port = 8080;
@@ -10,7 +15,9 @@ let travelData = {
   latitude: "",
   longitude: "",
   country: "",
-  when: ""
+  when: "",
+  response: {},
+  image: {}
 };
 
 //initializing an instance of our app
@@ -19,7 +26,10 @@ const app = express();
 app.use(express.static("dist"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 //letting un know on which port we run the app
 app.listen(port, () => console.log(`App running on prot ${port}`));
 
@@ -38,8 +48,57 @@ app.post("/travel-info", (req, res) => {
   travelData.country = country;
   travelData.latitude = latitude;
   travelData.longitude = longitude;
-  res.send("added");
+  getWeather(latitude, longitude, travelData.when);
+  getImage(city, country);
+  res.send(travelData);
 });
 app.post("/travel-info-date", (req, res) => {
   travelData.when = req.body.when;
 });
+
+async function getWeather(latitude, longitude, when) {
+  if (when == "this week") {
+    const data = await fetch(
+      `https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/${latitude},${longitude}`
+    );
+    const response = await data.json();
+    const relevantData = response.daily.data[7];
+    const { temperatureLow, temperatureHigh, summary } = relevantData;
+
+    travelData.response = {
+      low: `${((temperatureLow - 32) / 1.8).toFixed(1)} celsius`,
+      high: `${((temperatureHigh - 32) / 1.8).toFixed(1)} celsius`,
+      summary: summary
+    };
+  } else {
+    const data = await fetch(
+      `https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/${latitude},${longitude},${when}`
+    );
+    const response = await data.json();
+    const relevantData = response.daily.data[0];
+    const { temperatureLow, temperatureHigh, summary } = relevantData;
+    travelData.response = {
+      low: `${((temperatureLow - 32) / 1.8).toFixed(1)} celsius`,
+      high: `${((temperatureHigh - 32) / 1.8).toFixed(1)} celsius`,
+      summary: summary
+    };
+  }
+}
+
+async function getImage(city) {
+  const text = `${city}+city`;
+  const response = await fetch(
+    `${pixabayBase}${process.env.PIXABAY_KEY}&q=${text}&image_type=photo`
+  );
+  const data = await response.json();
+  const imageData = data.hits[0];
+  if (imageData) {
+    travelData.image = {
+      imageLink: imageData.webformatURL
+    };
+  } else {
+    travelData.image = {
+      imageLink: "not found"
+    };
+  }
+}
